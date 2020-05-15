4fd5406c8c99fb46835d0677dfbdec3fb7807e55
Kashinath Patekar <42115482+stellarbeam@users.noreply.github.com>
2020-05-15 22:57:54 +0530

    Set colors for mask

--- mask_classifier/Data_Generator/loop_through_folder.py
+++ mask_classifier/Data_Generator/loop_through_folder.py
@@ -10,7 +10,9 @@ args = parser.parse_args()
 folder_path = args.path
 print("Target folder: ", folder_path)
 
+colors = [ "default", "black", "blue", "red" ]
+
 images = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
 for i in range(len(images)):
-    print("the path of the image is", images[i])
-    create_mask(images[i])
+    # print("the path of the image is", images[i])
+    create_mask(images[i], colors[i%len(colors)] )
