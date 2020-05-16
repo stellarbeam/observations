26d2a479f7b825a5c3c8d36896f6b8e6a16dae94
Kashinath Patekar <42115482+stellarbeam@users.noreply.github.com>
2020-05-16 10:59:54 +0530

    Create mask of every color for each image

--- mask_classifier/Data_Generator/loop_through_folder.py
+++ mask_classifier/Data_Generator/loop_through_folder.py
@@ -14,5 +14,5 @@ colors = [ "default", "black", "blue", "red" ]
 
 images = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
 for i in range(len(images)):
-    # print("the path of the image is", images[i])
-    create_mask(images[i], colors[i%len(colors)] )
+    for color in colors:
+      create_mask(images[i], color )
